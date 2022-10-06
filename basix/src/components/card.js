98a2955301a059443eed27daf8cb8df7721f7f3a
Saurabh1499 <90036775+SaurabhMulay999@users.noreply.github.com>
2022-10-06 15:18:47 +0530

    DOne

--- basix/src/components/card.js
+++ basix/src/components/card.js
@@ -7,7 +7,7 @@ function card(props){
     const classes='card '+props.className;
 
     return (
-        <div className='card'>{props.children}</div>
+        <div className={classes}>{props.children}</div>
 
     );
 }
